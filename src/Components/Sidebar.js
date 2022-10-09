5fc5bfa248fc188563c059bfa74be1b0a18cd003
Eric <ericlg.link@gmail.com>
2022-10-09 11:04:21 -0700

    List styled for Sidebar

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -1,14 +1,21 @@
 import Help from './Help'
 
+const liStyle = {
+    'display' : 'inline',
+    'margin' : '3px',
+    'padding' : '1px',
+    'border' : '2px solid black'
+}
+
 const Sidebar = (props) => {
     return (
         <div className="sidebar">
             <h3>Listings</h3>
             <div>
                 <ul>
-                    <li>all</li>
-                    <li>owner</li>
-                    <li>dealer</li>
+                    <li style={liStyle}>all</li>
+                    <li style={liStyle}>owner</li>
+                    <li style={liStyle}>dealer</li>
                 </ul>
             </div>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt alias hic delectus eligendi aliquam dolores velit eos repudiandae repellat odit iure itaque, ex iste assumenda aspernatur quo voluptate, mollitia illum?</p>
